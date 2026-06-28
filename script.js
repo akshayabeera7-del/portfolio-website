ac65c0b9d948c746502c5468a4b46a27e4645c86
Beera Akshaya <sivalakshmia7@gmail.com>
2026-06-28 19:30:50 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,70 @@
+// Smooth scroll animation
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Fade animation
+const sections = document.querySelectorAll("section");
+
+// Initially hide all sections
+sections.forEach(section => {
+    section.style.opacity = "0";
+    section.style.transform = "translateY(50px)";
+    section.style.transition = "all 0.8s ease";
+});
+
+// Home section ni page load ayye ventane chupinchu
+const home = document.querySelector("#home");
+home.style.opacity = "1";
+home.style.transform = "translateY(0)";
+
+// Migatha sections scroll appudu show cheyyi
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
+
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.onclick = function () {
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+        themeBtn.innerHTML = "☀️";
+    }else{
+        themeBtn.innerHTML = "🌙";
+    }
+};
