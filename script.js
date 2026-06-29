e476628a5edfc0b64d18e1fb6562e3230443bf3d
Beera Akshaya <sivalakshmia7@gmail.com>
2026-06-29 17:12:16 +0530

    updated portfolio

--- script.js
+++ script.js
@@ -68,3 +68,12 @@ themeBtn.onclick = function () {
         themeBtn.innerHTML = "🌙";
     }
 };
+
+// Hamburger Menu
+
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
